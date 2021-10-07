a24011a61c8f98781f27fa36e6513f1289b6ec69
Iordan Tonchev <issashu@icloud.com>
2021-10-07 10:39:51 +0300

    Initial commit of the project.

---
+++ build/CMakeFiles/SDL_game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL_game.dir/src/event_poll.c.o"
+  "CMakeFiles/SDL_game.dir/src/event_poll.c.o.d"
+  "CMakeFiles/SDL_game.dir/src/main.c.o"
+  "CMakeFiles/SDL_game.dir/src/main.c.o.d"
+  "CMakeFiles/SDL_game.dir/src/sdl_methods.c.o"
+  "CMakeFiles/SDL_game.dir/src/sdl_methods.c.o.d"
+  "SDL_game"
+  "SDL_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SDL_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/defines.h
@@ -0,0 +1,14 @@
+//
+// Created by Iordan Tonchev on 6.10.21.
+//
+
+#ifndef SDL_GAME_DEFINES_H
+#define SDL_GAME_DEFINES_H
+
+#define SUCCESS 0
+#define FAILURE 1
+#define STRING char*
+#define TRUE 1
+#define FALSE 0
+
+#endif //SDL_GAME_DEFINES_H
