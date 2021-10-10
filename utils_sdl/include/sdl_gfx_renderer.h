2bd0b367969af579c5e8e5999471d0dc9a27228b
Iordan Tonchev <issashu@icloud.com>
2021-10-10 22:19:57 +0300

    Linux compiles and runs version

---
+++ utils_sdl/include/sdl_gfx_renderer.h
@@ -0,0 +1,8 @@
+//
+// Created by Iordan Tonchev on 10.10.21.
+//
+
+#ifndef SDL_GAME_SDL_GFX_RENDERER_H
+#define SDL_GAME_SDL_GFX_RENDERER_H
+
+#endif //SDL_GAME_SDL_GFX_RENDERER_H
---
+++ utils_sdl/src/sdl_gfx_renderer.c
@@ -0,0 +1,5 @@
+//
+// Created by Iordan Tonchev on 10.10.21.
+//
+
+#include "sdl_gfx_renderer.h"
